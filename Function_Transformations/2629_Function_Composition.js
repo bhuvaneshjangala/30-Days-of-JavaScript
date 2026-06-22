915616af8619cb1b72450d913cc2602bcbee88dc
Bhuvanesh Jangala <jangalabhuvanesh24@gmail.com>
2026-06-22 15:23:37 +0530

    Update 2629_Function_Composition.js

--- Function_Transformations/2629_Function_Composition.js
+++ Function_Transformations/2629_Function_Composition.js
@@ -1,10 +1,21 @@
-/*
-LeetCode Problem:
-Problem Number: 2629
-Difficulty:
-Approach:
-Time Complexity:
-Space Complexity:
-*/
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    
+    return function(x) {
+        let result = x;
 
-// Implement solution here
+        for (let i = functions.length - 1; i >= 0; i--) {
+            result = functions[i](result);
+        }
+
+        return result;
+    };
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
