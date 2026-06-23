f734b408a5f5dac267c289cafd1dcc27a4baea54
Bhuvanesh Jangala <jangalabhuvanesh24@gmail.com>
2026-06-23 09:50:10 +0530

    Update 2694_Event_Emitter.js

--- Classes/2694_Event_Emitter.js
+++ Classes/2694_Event_Emitter.js
@@ -1,10 +1,37 @@
-/*
-LeetCode Problem:
-Problem Number: 2694
-Difficulty:
-Approach:
-Time Complexity:
-Space Complexity:
-*/
+class Calculator {
+    constructor(value) {
+        this.result = value;
+    }
 
-// Implement solution here
+    add(value) {
+        this.result += value;
+        return this;
+    }
+
+    subtract(value) {
+        this.result -= value;
+        return this;
+    }
+
+    multiply(value) {
+        this.result *= value;
+        return this;
+    }
+
+    divide(value) {
+        if (value === 0) {
+            throw new Error("Division by zero is not allowed");
+        }
+        this.result /= value;
+        return this;
+    }
+
+    power(value) {
+        this.result **= value;
+        return this;
+    }
+
+    getResult() {
+        return this.result;
+    }
+}
