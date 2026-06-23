efb4002b6b47cc7c82cb4457a04ee3d9e2961186
Bhuvanesh Jangala <jangalabhuvanesh24@gmail.com>
2026-06-23 09:48:43 +0530

    Update 2695_Array_Wrapper.js

--- Classes/2695_Array_Wrapper.js
+++ Classes/2695_Array_Wrapper.js
@@ -1,10 +1,29 @@
-/*
-LeetCode Problem:
-Problem Number: 2695
-Difficulty:
-Approach:
-Time Complexity:
-Space Complexity:
-*/
+/**
+ * @param {number[]} nums
+ * @return {void}
+ */
+var ArrayWrapper = function(nums) {
+    this.nums = nums;
+};
 
-// Implement solution here
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function() {
+    return this.nums.reduce((sum, num) => sum + num, 0);
+};
+
+/**
+ * @return {string}
+ */
+ArrayWrapper.prototype.toString = function() {
+    return `[${this.nums.join(",")}]`;
+};
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
