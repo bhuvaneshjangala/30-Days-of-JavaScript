4265ddf7f5a62185a537ef5ae3ef004f1e5f5618
Bhuvanesh Jangala <jangalabhuvanesh24@gmail.com>
2026-06-24 22:02:59 +0530

    Update 2723_Add_Two_Promises.js

--- Promises_And_Time/2723_Add_Two_Promises.js
+++ Promises_And_Time/2723_Add_Two_Promises.js
@@ -1,10 +1,14 @@
-/*
-LeetCode Problem:
-Problem Number: 2723
-Difficulty:
-Approach:
-Time Complexity:
-Space Complexity:
-*/
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    const [value1, value2] = await Promise.all([promise1, promise2]);
+    return value1 + value2;
+};
 
-// Implement solution here
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
