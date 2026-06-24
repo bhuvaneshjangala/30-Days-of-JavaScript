473b25f701cf6933f747057de001b7e6c1d1a38b
Bhuvanesh Jangala <jangalabhuvanesh24@gmail.com>
2026-06-24 22:07:57 +0530

    Update 2715_Timeout_Cancellation.js

--- Promises_And_Time/2715_Timeout_Cancellation.js
+++ Promises_And_Time/2715_Timeout_Cancellation.js
@@ -1,10 +1,34 @@
-/*
-LeetCode Problem:
-Problem Number: 2715
-Difficulty:
-Approach:
-Time Complexity:
-Space Complexity:
-*/
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
 
-// Implement solution here
+    const timeoutId = setTimeout(() => {
+        fn(...args);
+    }, t);
+
+    return function() {
+        clearTimeout(timeoutId);
+    };
+};
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 5;
+ *  const args = [2], t = 20, cancelTimeMs = 50;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  setTimeout(cancel, cancelTimeMs);
+ */
