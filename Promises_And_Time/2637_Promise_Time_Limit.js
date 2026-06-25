c8cfb26da4deebf3c212644802e4b4597c5bb321
Bhuvanesh Jangala <jangalabhuvanesh24@gmail.com>
2026-06-25 22:05:08 +0530

    Update 2637_Promise_Time_Limit.js

--- Promises_And_Time/2637_Promise_Time_Limit.js
+++ Promises_And_Time/2637_Promise_Time_Limit.js
@@ -1,10 +1,24 @@
-/*
-LeetCode Problem:
-Problem Number: 2637
-Difficulty:
-Approach:
-Time Complexity:
-Space Complexity:
-*/
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
 
-// Implement solution here
+    return async function(...args) {
+
+        const timeoutPromise = new Promise((_, reject) => {
+            setTimeout(() => reject("Time Limit Exceeded"), t);
+        });
+
+        return Promise.race([
+            fn(...args),
+            timeoutPromise
+        ]);
+    };
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded"
+ */
