6f96a4c545a3a5f2214c9e69097f63e720c7897b
Bhuvanesh Jangala <jangalabhuvanesh24@gmail.com>
2026-06-28 21:52:45 +0530

    Update 2627_Debounce.js

--- Promises_And_Time/2627_Debounce.js
+++ Promises_And_Time/2627_Debounce.js
@@ -1,4 +1,29 @@
-/*
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+
+    let timeoutId;
+
+    return function(...args) {
+
+        clearTimeout(timeoutId);
+
+        timeoutId = setTimeout(() => {
+            fn(...args);
+        }, t);
+
+    }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ *//*
 LeetCode Problem:
 Problem Number: 2627
 Difficulty:
