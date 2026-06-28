19beebc645482af6278aeba42ba0d921d820c15e
Bhuvanesh Jangala <jangalabhuvanesh24@gmail.com>
2026-06-28 22:06:49 +0530

    Update 2727_Is_Object_Empty.js

--- JSON/2727_Is_Object_Empty.js
+++ JSON/2727_Is_Object_Empty.js
@@ -1,4 +1,10 @@
-/*
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    return Object.keys(obj).length === 0;
+};/*
 LeetCode Problem:
 Problem Number: 2727
 Difficulty:
